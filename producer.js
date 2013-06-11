fc242484a18ae920fa23cbdf35f1fc2db1e68afa
Mark Rushton <mark@modernfidelity.co.uk>
2013-06-11 20:56:06 +0100

    first commit

---
+++ producer.js
@@ -0,0 +1,26 @@
+/**
+ * ZEROMQ + NODEJS
+ *
+ * Producer.js - A simple pub sub demo
+ *
+ */
+
+var zmq = require('zmq')
+    , sock = zmq.socket('push');
+
+sock.bindSync('tcp://127.0.0.1:3000');
+
+console.log('Producer bound to port 3000');
+
+setInterval(function(){
+
+
+    console.log('sending work');
+
+    var myDate=new Date();
+
+    sock.send('some work=' + myDate);
+
+
+
+}, 1000);
---
+++ worker.js
@@ -0,0 +1,16 @@
+/**
+ * ZEROMQ + NODEJS
+ *
+ * Worker.js - A simple pub sub demo
+ *
+ */
+
+var zmq = require('zmq')
+    , sock = zmq.socket('pull');
+
+sock.connect('tcp://127.0.0.1:3000');
+console.log('Worker connected to port 3000');
+
+sock.on('message', function(msg){
+    console.log('work: %s', msg.toString());
+});
